0de429a546c8ceda24e79653133dd295216d55b6
Alexandre <alexandre@jsc-trade?com>
2022-11-12 12:25:55 +0100

    [ADD] express ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    }
+);
+
+app.listen(3000, () => {
+    console.log("My app is listening on port " + 3000);
+});
