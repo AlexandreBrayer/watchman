f7f273abe3123a4669a80c225c353a52b30e3bc0
Alexandre <alexandre@jsc-trade?com>
2022-11-23 11:29:03 +0100

    [ADD] basic conf for express app + flux route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
+require("dotenv").config();
 import express from "express";
-
+import cors from "cors";
+import bodyParser from "body-parser";
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    }
-);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+import fluxRoute from "./routes/flux";
+app.use("/flux", fluxRoute);
+
+app.get("/", async (req, res) => {
+    res.json({version: "0.0.1"});
+});
 
 app.listen(3000, () => {
-    console.log("My app is listening on port " + 3000);
+  console.log("My app is listening on port " + port);
 });
---
+++ src/routes/flux.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+
+const PocketBase = require("pocketbase/cjs");
+const pb = new PocketBase(process.env.POCKET_BASE_URL);
+const connect = async () => {
+  console.log("Connecting to PocketBase...");
+  await pb.admins.authWithPassword(
+    process.env.ADMIN_EMAIL,
+    process.env.ADMIN_PASSWORD
+  );
+};
+connect();
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const result = await pb.collection("flux").getFullList(1000000);
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.get("/search/:q", async (req: Request, res: Response) => {
+    try {
+        const result = await pb.collection("flux").getList(1,50, {
+            filter: 'name ~ "' + req.params.q + '"'
+        });
+        res.status(200).json(result);
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+})
+
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pb.collection("flux").getOne(req.params.id);
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(404).json({ error: "Not found" });
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const result = await pb.collection("flux").create(req.body);
+    res.status(201).json(result);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    await pb.collection("flux").update(req.params.id, req.body);
+    res.status(204).json();
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+export default router;
