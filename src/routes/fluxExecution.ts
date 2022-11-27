bd71d9f025976cb1719dabf4d9aac83acb7520d8
Alexandre <alexandre@jsc-trade?com>
2022-11-27 11:05:42 +0100

    [ADD] data type for buffer array + removed logs

--- src/routes/fluxExecution.ts
+++ src/routes/fluxExecution.ts
@@ -12,10 +12,11 @@ router.post("/", async (req: Request, res: Response) => {
     const fluxid = req.body.id;
     const processes = await Process.find({ flux: fluxid });
     const processIds = processes.map((p: any) => p.id);
-    console.log(processIds);
-    var buffer: Array<any> = [];
+    var buffer: Array<String> = [];
     const core = spawn(python, [path, ...processIds]);
+
     res.status(201).json({ message: "Flux Spawned" });
+    
     core.stdout.on("data", async (data: any) => {
       buffer.push(data.toString());
     });
@@ -23,7 +24,6 @@ router.post("/", async (req: Request, res: Response) => {
       const result = buffer.join("");
         const products = JSON.parse(result);
         await Product.insertMany(products);
-        console.log("MAIS NON ??")
     });
   } catch (e) {
     res.status(500).json({ error: e });
