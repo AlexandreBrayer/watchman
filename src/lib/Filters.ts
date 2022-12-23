3d569be3d81ad5444cf029f85f1504b76ea943ee
Alexandre <alexandre@jsc-trade?com>
2022-12-23 15:16:31 +0100

    [ADD] you can now have multiple searchs for a field (array)

--- src/lib/Filters.ts
+++ src/lib/Filters.ts
@@ -1,17 +1,17 @@
 export function parseFilters(filters: any, dateBarrier: any) {
   const parsedFilters: any = {};
   for (const key in filters) {
-    if (
-      typeof filters[key].value === "string" &&
-      filters[key].strict === false
-    ) {
-      const regexValue = new RegExp(filters[key].value, "i");
-      parsedFilters[key] = regexValue;
+    console.log(filters[key].value);
+    if (Array.isArray(filters[key].value) && filters[key].strict === false) {
+      const regexValues = filters[key].value.map((value: any) => {
+        return new RegExp(value, "i");
+      });
+      parsedFilters[key] = { $in: regexValues };
     } else {
-      parsedFilters[key] = filters[key].value;
+      parsedFilters[key] = { $in: filters[key].value };
     }
   }
-  if (dateBarrier?.use) {
+  if (dateBarrier) {
     dateBarrier.after === true
       ? (parsedFilters.createdAt = { $gte: dateBarrier.value })
       : (parsedFilters.createdAt = { $lte: dateBarrier.value });
